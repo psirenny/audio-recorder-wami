3284c4d1b92a549ecf41b50e0d3e9d9a03385f45
psirenny <djtorres0@gmail.com>
2014-02-12 09:00:24 -0500

    make it work

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
-module.exports = function () {
+module.exports = function (options) {
   return {
     available: function (callback) {
       if (typeof navigator === 'undefined') return callback(false);
       return callback(!!navigator.mimeTypes['application/x-shockwave-flash']);
     },
     permission: function (callback) {
-      return callback(true);
+      this.rec = {};
+      Wami.setup({
+        id: options.elementId,
+        onReady: callback,
+        swfUrl: options.swfUrl
+      });
     },
     start: function (callback) {
+      var self = this;
+
+      var error = Wami.nameCallback(function (err) {
+        self.rec.callback(err[0]);
+      });
+
+      var stop =  Wami.nameCallback(function (data) {
+        self.rec.callback(null, data[0].url);
+      });
+
+      Wami.startRecording(options.uploadUrl, null, stop, error);
       return callback();
     },
     stop: function (callback) {
-      return callback();
+      this.rec.callback = callback();
+      Wami.stopRecording();
     }
   };
 };
